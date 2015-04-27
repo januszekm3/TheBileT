b41378a1161c6aa60798e9d51f73146e85c6d104
Jacek Marchewka <icarotka@gmail.com>
2015-04-27 18:16:56 +0200

    Init

---
+++ App_Code/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public enum ServiceCommand
+{
+    register = 0,
+    login = 1,
+
+    checkTicket = 3,
+    buyTicket = 4,
+
+    listTickets = 5
+}
---
+++ App_Code/Membership/MembershipUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheBilet.Membership
+{
+    public class MembershipUtils
+    {
+        public static String VAR_USERNAME = "username";
+        public static String VAR_PASSWORD = "password";
+        public static String VAR_EMAIL = "email";
+
+
+        public static String RegisterAccount(HttpContext context)
+        {
+            return parseArguments(context, ServiceCommand.register);
+        }
+
+        public static String Authenticate(HttpContext context)
+        {
+            return parseArguments(context, ServiceCommand.login);
+        }
+
+        public static String parseArguments(HttpContext context, ServiceCommand command)
+        {
+            try
+            {
+                switch (command)
+                {
+                    case ServiceCommand.register:
+                        return Membership.RegisterAccount(context.Request.Params[VAR_USERNAME], context.Request.Params[VAR_PASSWORD], context.Request.Params[VAR_EMAIL]);
+
+                    case ServiceCommand.login:
+                        return Membership.Authenticate(context.Request.Params[VAR_USERNAME], context.Request.Params[VAR_PASSWORD]);
+
+                    default:
+                        return "Unknown command";
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                throw new ServiceException("Some arguments are missing", ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                throw new ServiceException("Invalid argument format", ex.Message);
+            }
+        }
+    }
+}
